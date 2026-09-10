b933765bcb7337442a8c9825e8d5f85fd16f5d21
Ayu-shhhh <ayushsrivastava09032004@gmail.com>
2026-09-10 18:21:02 +0530

    feat: handle peer disconnection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::net::TcpListener;
 use p2pnet::protocol;
 
+// use std::thread;
+// use std::time::Duration;
+
 fn main() -> Result<(), std::io::Error>
 {
 
@@ -10,10 +13,21 @@ fn main() -> Result<(), std::io::Error>
     let (mut stream, address) = listener.accept()?;
     println!("Connection from {}", address);
 
-    for _ in 0..3
+    loop
     {
-        let payload = protocol::read_message(&mut stream)?;
-        println!("Payload bytes: {:?}", str::from_utf8(&payload).unwrap());
+        // let payload;
+        match protocol::read_message(&mut stream)?
+        {
+            Some(payload) => {
+                println!("Payload bytes: {:?}", str::from_utf8(&payload).unwrap());
+            }
+            None => {
+                println!("Peer disconnected.");
+                break;
+            }
+        }
+        // let duration = Duration::from_secs(2);
+        // thread::sleep(duration);
     }
 
     
--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -1,15 +1,27 @@
 use std::io::{Read, Write};
 
-pub fn read_message(stream: &mut impl Read) -> Result<Vec<u8>, std::io::Error>
+pub fn read_message(stream: &mut impl Read) -> Result< Option<Vec<u8>> , std::io::Error>
 {
         let mut message_length_buffer = [0u8; 4];
-        stream.read_exact(&mut message_length_buffer)?;
+
+        let mut first_byte = [0u8; 1];
+        let bytes_read = stream.read(&mut first_byte)?;
+
+        if bytes_read == 0 {
+                return Ok(None);
+        }
+
+        message_length_buffer[0] = first_byte[0];
+        stream.read_exact(&mut message_length_buffer[1..])?;
+
+        // stream.read_exact(&mut message_length_buffer)?;
+        
         let length = usize::try_from(u32::from_be_bytes(message_length_buffer)).unwrap();
 
         let mut payload = vec!(0u8; length);
         stream.read_exact(&mut payload)?;
 
-        Ok(payload)
+        Ok(Some(payload))
 }
 
 pub fn write_message(stream: &mut impl Write, message: &[u8]) -> Result<(), std::io::Error>
